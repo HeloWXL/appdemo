79e8b1a8e142e585af081ea030ab4f98f78cd941
王咸林 <756316064@qq.com>
2019-08-25 11:04:59 +0800

    代码修改

--- src/main/java/com/qztc/appdemo/controller/LeaveController.java
+++ src/main/java/com/qztc/appdemo/controller/LeaveController.java
@@ -5,13 +5,15 @@ import com.qztc.appdemo.model.Leave;
 import com.qztc.appdemo.service.LeaveService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.Map;
 
 /**
- * @author wangxl
+ * @author xiayj
  * @ClassName LeaveController
  * @Description TODO
  * @date 2019/8/23 17:51
@@ -19,7 +21,7 @@ import java.util.Map;
  */
 @Api(tags = "请假接口")
 @RequestMapping("/leaveApi")
-@RestController
+@Controller
 public class LeaveController {
 
   @Resource
@@ -27,6 +29,7 @@ public class LeaveController {
 
   @ApiOperation(value = "添加一条请假记录")
   @PostMapping("/insertSelective")
+  @ResponseBody
   public DataResult<Integer> insertSelective(@RequestBody Leave leave){
     DataResult<Integer> result = new DataResult<>();
     result.setBody(leaveService.insertSelective(leave));
@@ -34,17 +37,26 @@ public class LeaveController {
   }
 
   @ApiOperation(value = "根据id查询请假记录")
-  @GetMapping("/selectByPrimaryKey")
-  public DataResult<Leave> selectByPrimaryKey(@RequestParam("leaveId") Integer leaveId){
-    DataResult<Leave> result = new DataResult<>();
+  @GetMapping("/selectByPrimaryKey/{leaveId}")
+  public String selectByPrimaryKey(@PathVariable("leaveId") Integer leaveId, Model model){
     Leave leave = leaveService.selectByPrimaryKey(leaveId);
-    result.setBody(leave);
-    return result;
+    model.addAttribute("leave",leave);
+    return "/student/leavedetail.html";
   }
 
 
-  @ApiOperation(value = "查询请假信息")
+  @ApiOperation(value = "根据ids查询请假记录")
+  @GetMapping("/selectByPrimaryKeys/")
+  @ResponseBody
+  public Leave selectByPrimaryKey(@RequestParam("leaveId") Integer leaveId){
+    Leave leave = leaveService.selectByPrimaryKey(leaveId);
+    return leave;
+  }
+
+
+  @ApiOperation(value = "根据学号查询请假信息")
   @GetMapping("/selectLeaveByPage")
+  @ResponseBody
   public DataResult<Map<String,Object>> selectLeaveByPage(@RequestParam("pageNo") Integer pageNo,
                                                           @RequestParam("pageSize") Integer pageSieze, @RequestParam("studentSno") Integer studentSno){
     DataResult<Map<String,Object>> result = new DataResult<>();
